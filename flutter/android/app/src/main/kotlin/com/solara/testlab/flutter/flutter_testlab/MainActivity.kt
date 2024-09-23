93c14ece2064bba8d4da6adeee9f763bd6219eae
Malek Kamel <sha.kamel.eng@gmail.com>
2024-09-24 02:09:46 +0300

    Initial commit

---
+++ flutter/android/app/src/main/kotlin/com/solara/testlab/flutter/flutter_testlab/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.solara.testlab.flutter.flutter_testlab
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ ios/IOSTestLab/IOSTestLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSTestLabApp.swift
+//  IOSTestLab
+//
+//  Created by Shaban on 24/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct IOSTestLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
